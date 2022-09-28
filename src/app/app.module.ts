1104a7dfa5ba13b8490445e1b58de277febdbf93
Pujan Budhathoki <PBudhathoki@firstcommand.com>
2022-09-28 06:58:57 -0500

    Implemented router with 4 routes, could be used for future boilerplate.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,53 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
+import {HomeComponent} from './routes/home.component';
+import {BasicComponent} from './routes/basic.component';
+import {ContainedComponent} from './routes/contained.component';
+import {AdvancedComponent} from './routes/advanced.component';
+
+const animationRoutes:Routes = [
+  {
+    path: 'animation-home',
+    component: HomeComponent
+  },
+  {
+    path: 'animation-basics',
+    component: BasicComponent
+  },
+  {
+    path: 'animation-contained',
+    component: ContainedComponent
+  },
+  {
+    path: 'animation-advanced',
+    component: AdvancedComponent
+  },
+  {
+    path: '',
+    redirectTo: '/animation-home',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+      AppComponent,
+      HomeComponent,
+      BasicComponent,
+      ContainedComponent,
+      AdvancedComponent
+    ],
+    imports: [
+        BrowserModule, 
+        RouterModule.forRoot(animationRoutes),
+        AppRoutingModule, 
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
